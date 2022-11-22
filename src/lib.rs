7b42dfe86bcc64ef91c0493226c84c50814ac4a4
mahmudulislam299 <mahmudulislam299@gmail.com>
2022-11-22 17:53:15 +0600

    lib file updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,3 @@
-//! THis is is test library
-//!
-//! you add , sub , mul , div two number using this create
-//! this is very useful library 
-//!
-//! # Quick Start
-//!
-//! To get you started quickly, the easiest and highest-level way to get
-//! a random value is to use [`random()`]; alternatively you can use
-//! [`thread_rng()`]. The [`Rng`] trait provides a useful API on all RNGs, while
-//! the [`distributions`] and [`seq`] modules provide further
-//! functionality on top of RNGs.
-//!
-//! # The Book
-//!
-//! For the user guide and further documentation, please read
-//! [The Rust Rand Book](https://rust-random.github.io/book)
-
-
-
 pub mod mul_div;
 pub mod struct_test;
 
